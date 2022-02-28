2f1f95e1bc188ab10235eba2b0618e0bc8a10ca8
Daniel Souza <danielfsouza@gmail.com>
2022-02-28 14:19:44 -0300

    JPA Dependency

---
+++ src/main/java/com/educandoweb/curso/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.educandoweb.curso.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.curso.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+		
+}
